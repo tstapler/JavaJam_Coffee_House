27a7da96f2db6c44b3af538fd36b2cf5d7aa7bc6
Tyler Stapler <tystapler@gmail.com>
2017-11-13 10:37:14 -0600

    Add datafiles for JavaJam

--- gen_new_folder.py
+++ gen_new_folder.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import os
 import re
+import shutil
 
-parser = argparse.ArgumentParser(description='Generate new folders for COMS106')
+parser = argparse.ArgumentParser(
+    description='Generate new folders for COMS106')
 parser.add_argument('files', metavar='FILE', nargs='+')
 
 args = parser.parse_args()
@@ -19,6 +20,6 @@ last_week = max(files, key=lambda x: list(map(int, re.findall('\d+', x))))
 last_week_number = re.findall('\d+', last_week)[1]
 
 this_week = 'TylerStaplerG2WK{}P'.format(int(last_week_number) + 1)
-print("Making {}", this_week)
+print("Making {}".format(this_week))
 
-os.makedirs(this_week)
+shutil.copytree(last_week, this_week)
